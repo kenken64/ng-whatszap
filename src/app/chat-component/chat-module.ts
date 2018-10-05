19602deda02c5af7b5c92db968af933e98da13af
Kenneth Phang <bunnyppl@gmail.com>
2018-10-05 20:19:56 +0800

    added content loeader

--- src/app/chat-component/chat-module.ts
+++ src/app/chat-component/chat-module.ts
@@ -146,12 +146,8 @@ const routes = [
     AngularFireStorageModule,
     AngularFireMessagingModule,
     PerfectScrollbarModule,
-<<<<<<< HEAD
     WebCamModule,
     ContentLoaderModule
-=======
-    WebCamModule
->>>>>>> 257a9c87fab7a78af3656e5624b0dda3657aa628
   ],
   entryComponents: [DialogOverviewExampleDialog, WebcamDialog],
   declarations: [
