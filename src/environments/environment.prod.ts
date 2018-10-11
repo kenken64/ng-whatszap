f1a7a9083bc5e7ccb9347e43d5e468effefae9b8
unknown <bunnyppl@hotmail.com>
2018-10-11 23:26:26 +0800

    added sentiment analysis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,5 +12,5 @@ export const environment = {
   wsserver: 'https://mighty-bastion-61444.herokuapp.com/',
   firebase_cms_url: `https://firebasestorage.googleapis.com/v0/b/chat-app-acea7.appspot.com/o/`,
   firebase_cms_url_postfix: '?alt=media&token=0bb313a8-2b7b-4b59-af44-5e1044376e0e',
-  sentiment_api: 'https://blooming-fjord-73499.herokuapp.com/'
+  sentiment_api: 'https://blooming-fjord-73499.herokuapp.com'
 };
