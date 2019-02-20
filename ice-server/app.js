0d9f7c0cb5b2b61876e781a84aa1a8448a91d36b
unknown <bunnyppl@hotmail.com>
2019-02-21 01:28:43 +0800

    added sentimental analysis and ice

---
+++ ice-server/app.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+var bodyParser=require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = app
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    .use(cors())
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+const accountSid = 'ACbf9e16d08c742aa969d7a4719280f9cf';
+const authToken = 'abb5003f2b71b27a6b229710ebce285a';
+
+const client = require('twilio')(accountSid, authToken);
+
+app.use('/getICETokens', function (req, res) {
+    client.tokens.create({}, function (err, token) {
+        console.log(token)
+        var response = JSON.parse(JSON.stringify(token.iceServers));
+        res.send(response);
+    });
+})
+
+io.sockets.on('connection', function (socket) {
+
+    // convenience function to log server messages on the client
+    function log() {
+        var array = ['Message from server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+    }
+
+    socket.on('message', function (message) {
+        log('Client said: ', message);
+        // for a real app, would be room-only (not broadcast)
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('create or join', function (room) {
+        log('Received request to create or join room ' + room);
+
+        var numClients = Object.keys(io.sockets.sockets).length;
+        log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+        if (numClients === 1) {
+            socket.join(room);
+            log('Client ID ' + socket.id + ' created room ' + room);
+            socket.emit('created', room, socket.id);
+
+        } else if (numClients === 2) {
+            log('Client ID ' + socket.id + ' joined room ' + room);
+            io.sockets.in(room).emit('join', room);
+            socket.join(room);
+            socket.emit('joined', room, socket.id);
+            io.sockets.in(room).emit('ready');
+        } else { // max two clients
+            socket.emit('full', room);
+        }
+    });
+
+    socket.on('ipaddr', function () {
+        var ifaces = os.networkInterfaces();
+        for (var dev in ifaces) {
+            ifaces[dev].forEach(function (details) {
+                if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+                    socket.emit('ipaddr', details.address);
+                }
+            });
+        }
+    });
+
+    socket.on('bye', function () {
+        console.log('received bye');
+    });
+
+});
---
+++ sentiment-analysis-server/app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require('cors');
+const uuidv5 = require('uuid/v5');
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+
+var serviceAccount = require('./credentials.json');
+const NODE_PORT = process.env.PORT;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://everythingaboutai-3f390.firebaseio.com"
+});
+
+const db = admin.firestore();
+const settings = { timestampsInSnapshots: true};
+db.settings(settings);
+const app = express();
+app.use(cors());
+const chats = db.collection('chat-sentiment-analysis');
+
+var observer = chats.onSnapshot( querySnapshot => {
+    console.log(`Received query snapshot of size ${querySnapshot.size}`);
+    querySnapshot.docChanges.forEach(async change => {
+      if (change.doc.data().label) return;
+      if (change.type === 'added') {
+        console.log('New : ', change.doc.data());
+        console.log(__dirname);
+        var result = sentiment.analyze(change.doc.data().text);
+        console.dir(result);
+        let newlabel = "positive";
+        if(result.score <= 0){
+            newlabel = "negative";
+        }
+        var updateData = {
+            id: change.doc.data().id,
+            text: change.doc.data().text,
+            from: change.doc.data().from,
+            label: newlabel
+        };
+        await chats.doc(change.doc.id).set(updateData).catch((err)=>console.log(err))
+      }
+    });
+// ...
+}, err => {
+    console.log(`Encountered error: ${err}`);
+});
+
+
+app.get('/add', (req, res)=>{
+    let textVal = req.query.text;
+    let fromWho = req.query.fromWho;
+    
+    let value = {
+        id: uuidv5('kennethphang.asia', uuidv5.DNS),
+        text: textVal,
+        from: fromWho,
+    }
+    chats.add(value).then((result)=>{
+        console.log("created!" + result);
+        res.status(200).json(result._referencePath.segments[1]);
+    }).catch((error)=>console.log(error));
+});
+
+app.get('/getAllSentiment', (req, res)=>{
+    let aiChats = [];
+    chats.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        aiChats.push(doc.data())
+      });
+      res.status(200).json(aiChats);
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+});
+
+app.listen(NODE_PORT, ()=>{
+    console.log(`Listening ...${NODE_PORT}`);
+})
